64695b49eccd32b74f56d132dfe6f30b66926796
yamano <yamano@mobile.com>
2023-05-10 15:39:34 +0000

    downloaded markdown2 and few changes

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,20 @@
+import markdown2
 from django.shortcuts import render
-
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+def getpage(request, pagename):
+    html = markdown2.markdown(util.get_entry(pagename))
+    if html:
+        return render(request, html)
+    else:
+        return render(request, "encyclopedia/404.html", {
+"pagename": pagename
     })
 
