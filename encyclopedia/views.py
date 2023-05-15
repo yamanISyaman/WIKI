af3b8f7a9ddc79d41ee709e7c114269b7b9d6013
yamanomobile <yamano@mobile.com>
2023-05-15 10:35:08 +0000

    project complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -42,7 +42,7 @@ def index(request):
 
 
 def getpage(request, pagename):
-    
+
     html = util.get_entry(pagename)
     if html:
         return render(request, "encyclopedia/entries.html", {
@@ -51,7 +51,8 @@ def getpage(request, pagename):
             "data": ""
         })
     else:
-        return render(request, "encyclopedia/404.html", {"error": f'''the page \"{pagename}\" was not found'''})
+        return render(request, "encyclopedia/404.html",
+                      {"error": f'''the page \"{pagename}\" was not found'''})
 
 
 def add_entry(request):
@@ -60,30 +61,35 @@ def add_entry(request):
         entry = request.POST.get("entry")
         ls = [i.upper() for i in util.list_entries()]
         if title.upper() in ls:
-            return render(request, "encyclopedia/404.html", {"error": f'''An entry with the title \"{title}\" already exists'''})
+            return render(request, "encyclopedia/404.html", {
+                "error":
+                f'''An entry with the title \"{title}\" already exists'''
+            })
         util.save_entry(title, entry)
         return HttpResponseRedirect(reverse("index"))
-    return render(request, "encyclopedia/entrypage.html", {
-        "title": "Add Entry",
-        "form": EntryTextForm(),
-        "button": "Add",
-        "url": "addentry",
-    })
+    return render(
+        request, "encyclopedia/entrypage.html", {
+            "title": "Add Entry",
+            "form": EntryTextForm(),
+            "button": "Add",
+            "url": "addentry",
+        })
 
 
 def edit_entry(request, name):
     if request.method == "POST":
         title = name
-        entry = request.POST.get("entry")        
+        entry = request.POST.get("entry")
         util.save_entry(title, entry)
         return HttpResponseRedirect(reverse("index"))
-    return render(request, "encyclopedia/entrypage.html", {
-        "title": "Edit Entry",
-        "button": "Edit",
-        "url": "edit",
-        "data": util.get_entry(name),
-        "name": name
-    })
+    return render(
+        request, "encyclopedia/entrypage.html", {
+            "title": "Edit Entry",
+            "button": "Edit",
+            "url": "edit",
+            "data": util.get_entry(name),
+            "name": name
+        })
 
 
 def rand_page(request):
